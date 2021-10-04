56d049b63d993499b85520cd78e361c3060ba5e1
Andrew Browne <andrew.browne@contino.io>
2021-10-05 09:21:00 +1100

    edited tf state file

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
-# module "s3_bucket" {
-#   source = "./modules/s3"
-#   bucket = var.bucket
+module "s3_bucket" {
+  source = "./modules/s3"
+  bucket = var.bucket
 
-#   tags = var.tags
-# }
+  tags = var.tags
+}
 
-# output "bucket_name" {
-#   description = "The name of the bucket"
-#   value       = ["${module.s3_bucket.s3_bucket_name}"]
-# }
+output "bucket_name" {
+  description = "The name of the bucket"
+  value       = ["${module.s3_bucket.s3_bucket_name}"]
+}
 
-# output "bucket_name_arn" {
-#   description = "The name of the bucket"
-#   value       = ["${module.s3_bucket.s3_bucket_name_arn}"]
-# }
+output "bucket_name_arn" {
+  description = "The name of the bucket"
+  value       = ["${module.s3_bucket.s3_bucket_name_arn}"]
+}
 
 module "network" {
   source = "./modules/networking"
